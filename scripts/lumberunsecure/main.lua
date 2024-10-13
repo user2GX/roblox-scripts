a100de333123e2c8f1574bc174622a7c3d046ed8
2GX <121890173+user2GX@users.noreply.github.com>
2024-10-13 10:40:04 -0400

    Update main.lua

--- scripts/lumberunsecure/main.lua
+++ scripts/lumberunsecure/main.lua
@@ -1,6 +1,5 @@
 print("2gxHub loaded.")
 if game.PlaceId == 10042772221 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/user2GX/roblox-scripts/main/scripts/lumberunsecure/lu.lua"))()
-else
-    messagebox("2gxHub","This script was not made for this game! You are aware of the risks of getting banned if you run any of the features.", 0)
 end
+messagebox("2gxHub","This script was not made for this game! You are aware of the risks of getting banned if you run any of the features.", 0)
