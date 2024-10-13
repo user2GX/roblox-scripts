b3a147e6f1100551b5b71b2843149e0f4d8ee198
2GX <121890173+user2GX@users.noreply.github.com>
2024-10-13 11:05:00 -0400

    Update main.lua

--- scripts/lumberunsecure/main.lua
+++ scripts/lumberunsecure/main.lua
@@ -2,6 +2,9 @@ print("2gxHub loaded.")
 if game.PlaceId == 10042772221 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/user2GX/roblox-scripts/main/scripts/lumberunsecure/lu.lua"))()
 else
-    print("test")
-    printconsole("fuck you")
+    rconsolecreate()
+    rconsolename("2gxHub")
+    rconsoleprint('@@LIGHT_BLUE@@')
+    rconsoleprint(game:HttpGet('https://raw.githubusercontent.com/user2GX/roblox-scripts/main/scripts/lumberunsecure/hubmark.txt'))
+    rconsolewarn('This script was NOT made for this game. If you are banned from running any of our features in game, we are not responsible.')
 end
