41575ed641dc1b93bc9b46cd6b65c2771c7b86ae
2GX <121890173+user2GX@users.noreply.github.com>
2025-02-10 20:25:54 -0500

    some fixes

--- scripts/survivetheend/main.lua
+++ scripts/survivetheend/main.lua
@@ -1,5 +1,5 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/user2GX/roblox-scripts/main/scripts/survivetheend/source.lua')))()
-local Window = OrionLib:MakeWindow({Name = "Survive The End Of Roblox", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest", IntroEnabled = false})
+local Window = OrionLib:MakeWindow({Name = "Survive The End Of Roblox", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest", IntroText = "2gxHub"})
 
 --[[
 
