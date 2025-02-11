1d94fe70239a0d20b1492002f2d796401fc918c8
2GX <121890173+user2GX@users.noreply.github.com>
2025-02-11 16:10:18 -0500

    fix 2?

--- scripts/lumberunsecure/lu.lua
+++ scripts/lumberunsecure/lu.lua
@@ -164,8 +164,6 @@ local SettingsTab = Window:NewTab("Settings")
 local SettingsTab_UISection = SettingsTab:NewSection("UI")
 
 SettingsTab_UISection:NewButton("Reload UI", "Reloads the UI.", function()
-    if script.Parent.Parent:IsA("ScreenGui") then
-        script.Parent.Parent:Destroy()
-    end
+    Window:Destroy()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/user2GX/roblox-scripts/main/scripts/lumberunsecure/main.lua"))()
 end)
