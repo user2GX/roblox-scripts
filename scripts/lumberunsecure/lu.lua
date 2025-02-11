94b34c19555b0cad314ad6eda7c1ed6ab0333d60
2GX <121890173+user2GX@users.noreply.github.com>
2025-02-11 17:30:57 -0500

    remove .Name

--- scripts/lumberunsecure/lu.lua
+++ scripts/lumberunsecure/lu.lua
@@ -63,7 +63,7 @@ LandTab_TeleportSection:NewButton("Teleport to your plot", "Teleports you to you
     local player = game.Players.LocalPlayer
     for _, property in pairs(game.Workspace.Properties:GetChildren()) do
         local ownerValue = property:FindFirstChild("Owner")
-        if ownerValue and ownerValue.Value == player.Name then
+        if ownerValue and ownerValue.Value == player then
             local primaryPart = property.PrimaryPart
             if primaryPart then
                 player.Character.HumanoidRootPart.CFrame = primaryPart.CFrame + Vector3.new(0, 5, 0)
