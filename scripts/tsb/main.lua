b97e80a5d12480c9d6265922e465235f35db5a82
2GX <121890173+user2GX@users.noreply.github.com>
2025-02-12 11:21:15 -0500

    scr

--- scripts/tsb/main.lua
+++ scripts/tsb/main.lua
@@ -1,6 +1,40 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/user2GX/roblox-scripts/main/scripts/tsb/source.lua')))()
 local Window = OrionLib:MakeWindow({Name = "2gxHub", HidePremium = false, SaveConfig = true, ConfigFolder = "2gxHubConfig", IntroEnabled = false})
 
+-- vars
+
+local Players = game:GetService("Player")
+local Player = Players.LocalPlayer
+local PlayerGui = Player:FindFirstChild("PlayerGui")
+
+-- main tab
+local MainTab = Window:MakeTab({
+	Name = "Main"
+})
+
+local MainTab_Hotbar = Tab:AddSection({
+	Name = "Hotbar"
+})
+
+local HotbarGui = PlayerGui:FindFirstChild("Hotbar")
+local BackpackUI = HotbarGui:FindFirstChild("Backpack")
+local HotbarFrame = BackpackUI:FindFirstChild("Hotbar")
+local Hotbar_NonUlt = {
+    Option1 = HotbarFrame:FindFirstChild("1"),
+    Option2 = HotbarFrame:FindFirstChild("2"),
+    Option3 = HotbarFrame:FindFirstChild("3"),
+    Option4 = HotbarFrame:FindFirstChild("4")
+}
+
+MainTab_Hotbar:AddTextbox({
+	Name = "Ability 1",
+	Default = Hotbar_NonUlt.Option1.Text,
+	Callback = function(Value)
+		print(Value)
+        Hotbar_NonUlt.Option1.Text = value
+	end	  
+})
+
 --[[
 Name = <string> - The name of the UI.
 HidePremium = <bool> - Whether or not the user details shows Premium status or not.
