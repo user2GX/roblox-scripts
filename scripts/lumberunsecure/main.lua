df04db3c6f52efcd6c54be924edd272bd1c6aaa0
2GX <121890173+user2GX@users.noreply.github.com>
2025-02-12 11:03:18 -0500

    clipboard and notice

--- scripts/lumberunsecure/main.lua
+++ scripts/lumberunsecure/main.lua
@@ -14,6 +14,15 @@ Supported Games:
 
 ]]
 print("2gxHub loaded.")
+game.StarterGui:SetCore("SendNotification", {
+    Title = "2gxHub";
+    Text = "You are using the old loadstring! The new one was copied to your clipboard.";
+    Icon = "rbxassetid://89450855285797";
+    Duration = 5;
+})
+toclipboard("""
+loadstring(game:HttpGet("https://raw.githubusercontent.com/user2GX/roblox-scripts/main/main.lua"))()
+""")
 if game.PlaceId == 10042772221 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/user2GX/roblox-scripts/main/scripts/lumberunsecure/lu.lua"))()
 elseif game.PlaceId == 4261560642 then
