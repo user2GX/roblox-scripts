01879874126cc24a597dad040bcc995467c15b97
2GX <121890173+user2GX@users.noreply.github.com>
2025-02-12 10:58:57 -0500

    added the new place

--- scripts/lumberunsecure/main.lua
+++ scripts/lumberunsecure/main.lua
@@ -13,11 +13,13 @@ if game.PlaceId == 10042772221 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/user2GX/roblox-scripts/main/scripts/lumberunsecure/lu.lua"))()
 elseif game.PlaceId == 4261560642 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/user2GX/roblox-scripts/main/scripts/survivetheend/main.lua"))()
+elseif game.PlaceId == 10449761463 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/user2GX/roblox-scripts/main/scripts/tsb/main.lua"))()
 else
     game.StarterGui:SetCore("SendNotification", {
         Title = "2gxHub";
         Text = "2gxHub does not support this game!";
-        Icon = "rbxassetid://112162358356932";
+        Icon = "rbxassetid://89450855285797";
         Duration = 2;
     })
 end
