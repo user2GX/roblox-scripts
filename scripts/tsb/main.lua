2d35f613929b5bb42bc19cb026c27d77626f4990
2GX <121890173+user2GX@users.noreply.github.com>
2025-02-12 11:39:04 -0500

    add

--- scripts/tsb/main.lua
+++ scripts/tsb/main.lua
@@ -4,7 +4,7 @@ local Window = Rayfield:CreateWindow({
     Icon = "atom",
     LoadingTitle = "2gxHub",
     LoadingSubtitle = "by 2gx",
-    Theme = "Serenity",
+    Theme = "Dark",
  
     DisableRayfieldPrompts = false,
     DisableBuildWarnings = false,
@@ -14,4 +14,38 @@ local Window = Rayfield:CreateWindow({
        FolderName = nil,
        FileName = "2gxHubConfig"
     }
+ })
+
+-- variables
+
+local Players = game:GetService("Players")
+local Player = Players.LocalPlayer
+local PlayerGui = Player:WaitForChild("PlayerGui")
+local ScreenGuis = {
+    Hotbar = PlayerGui:WaitForChild("Hotbar")
+}
+
+local BackpackFrame = ScreenGuis.Hotbar:WaitForChild("Backpack")
+local HotbarFrame = BackpackFrame:WaitForChild("Hotbar")
+local HotbarAbilities_NonUlt = {
+    Ability1 = HotbarFrame:WaitForChild("1"),
+    Ability2 = HotbarFrame:WaitForChild("2"),
+    Ability3 = HotbarFrame:WaitForChild("3"),
+    Ability4 = HotbarFrame:WaitForChild("4")
+}
+
+-- main tab
+local MainTab = Window:CreateTab("Main", "blocks")
+local MainTab_Hotbar = MainTab:CreateSection("Hotbar")
+
+local Ability1Input = MainTab_Hotbar:CreateInput({
+    Name = "Ability 1",
+    CurrentValue = HotbarAbilities_NonUlt.Ability1.Text,
+    PlaceholderText = "Rapid Slice",
+    RemoveTextAfterFocusLost = false,
+    Flag = "Ability1Flag",
+    Callback = function(Text)
+        print(Text)
+        HotbarAbilities_NonUlt.Ability1.Text = Text
+    end,
  })
