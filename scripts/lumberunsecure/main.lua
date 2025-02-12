ea7b4e8a4dc47a449e369c2632f69ede15103e64
2GX <121890173+user2GX@users.noreply.github.com>
2025-02-12 11:03:33 -0500

    remove support

--- scripts/lumberunsecure/main.lua
+++ scripts/lumberunsecure/main.lua
@@ -23,17 +23,3 @@ game.StarterGui:SetCore("SendNotification", {
 toclipboard("""
 loadstring(game:HttpGet("https://raw.githubusercontent.com/user2GX/roblox-scripts/main/main.lua"))()
 """)
-if game.PlaceId == 10042772221 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/user2GX/roblox-scripts/main/scripts/lumberunsecure/lu.lua"))()
-elseif game.PlaceId == 4261560642 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/user2GX/roblox-scripts/main/scripts/survivetheend/main.lua"))()
-elseif game.PlaceId == 10449761463 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/user2GX/roblox-scripts/main/scripts/tsb/main.lua"))()
-else
-    game.StarterGui:SetCore("SendNotification", {
-        Title = "2gxHub";
-        Text = "2gxHub does not support this game!";
-        Icon = "rbxassetid://89450855285797";
-        Duration = 2;
-    })
-end
